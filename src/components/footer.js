7451bf86b414ff5565086a0654c62ba19abb04c8
Swapnil <swapmis1084@gmail.com>
2019-11-11 00:55:23 +0100

    Adding email info

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,9 @@ import React from "react"
 const Footer = () => (
   <footer className="text-xs text-gray-900">
     © {new Date().getFullYear()}, {` `} Anita Khemka
+    <a className="ml-6" href="mailto:contact@anitakhemka.com">
+      <span className="text-sm">✉</span> contact@anitakhemka.com
+    </a>
   </footer>
 )
 

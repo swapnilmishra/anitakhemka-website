24b796c539cb9b2c63618c761f13733ed465791b
Swapnil <swapmis1084@gmail.com>
2019-11-17 12:00:39 +0100

    Adding page for submission

---
+++ src/pages/4ff1586e356e45e2bb414fb240995553.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+
+export default () => {
+  return (
+    <Layout>
+      <SEO title="Coming soon" />
+      <div className="flex mt-12 justify-center">
+        <div>
+          <h3>Coming soon</h3>
+        </div>
+      </div>
+    </Layout>
+  )
+}

14f3ad06a4bd3eeecc17b2260bd08df69f3b6df2
Swapnil <swapmis1084@gmail.com>
2019-12-04 19:50:28 +0100

    add other subheaders

--- src/components/consts.js
+++ src/components/consts.js
@@ -46,8 +46,8 @@ export const sidenavItemsWomenDeliver = selectIndex => {
 export const sidenavItemsAbout = selectIndex => {
   let items = [
     { url: "/about/bio", title: "Biography" },
-    // { url: "/about/biography", title: "Exhibitions" },
-    // { url: "/about/biography", title: "Contact" },
+    { url: "/about/bio", title: "Exhibitions" },
+    { url: "/about/bio", title: "Contact" },
   ]
   if (typeof selectIndex !== "undefined") {
     items[selectIndex].selected = true

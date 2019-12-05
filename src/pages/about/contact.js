f1e8792b64c577b696afc7d35ace997d17f4e745
Swapnil <swapmis1084@gmail.com>
2019-12-05 16:28:45 +0100

    contact page

--- src/pages/about/contact.js
+++ src/pages/about/contact.js
@@ -15,16 +15,23 @@ const IndexPage = () => {
         <div className="w-2/12">
           <SideNav sidenavItems={sidenavItemsAbout(2)} />
         </div>
-        <div className="w-10/12 h-full text-sm text-center mt-10">
+        <div className="w-10/12 h-full text-sm mt-1">
           <div>
-            <a href="mailto:contact@anitakhemka.com">contact@anitakhemka.com</a>
+            <p className="font-semibold">Anita Khemka</p>
+            <a href="mailto:contact@anitakhemka.com">
+              <span className="text-sm">✉ </span>
+              <span>contact@anitakhemka.com</span>
+            </a>
           </div>
           <div className="mt-4">
-            <p>Gallery Representation PHOTOINK</p>
-            <a href="http://www.photoink.net">http://www.photoink.net</a>
-          </div>
-          <div className="mt-4">
-            <a href="mailto:info@photoink.net">info@photoink.net</a>
+            <p className="font-semibold">Gallery Representation PHOTOINK</p>
+            <p>
+              <a href="http://www.photoink.net">http://www.photoink.net</a>
+            </p>
+            <a href="mailto:info@photoink.net">
+              <span className="text-sm">✉ </span>
+              <span>info@photoink.net</span>
+            </a>
           </div>
         </div>
       </div>
